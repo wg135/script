58a35a7c50a93f2b6c703698497b189a2a8b0a83
gaowei0525 <gaowei0525@gmail.com>
2016-08-15 14:10:47 -0500

    port knowcking script

--- port_knocking.py
+++ port_knocking.py
@@ -7,11 +7,11 @@ ports = [1, 2, 3]
 
 ip_addr = "192.168.41.159"
 
-for dport in ports:
-    print ip_addr, dport
+for port in ports:
+    print ip_addr, port
     ip = IP(dst=ip_addr)
-    port = 39367
-    SYN = ip/TCP(sport=port, dport=dport, flags="S", window=2048, options=[('MSS',1460)], seq=0)
+    s_port = 39367
+    SYN = ip/TCP(sport=s_port, dport=port, flags="S", window=2048, options=[('MSS',1460)], seq=0)
     send(SYN)
     print "*KNOCK*"
    
